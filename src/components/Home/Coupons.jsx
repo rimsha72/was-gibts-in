253651e7bb0e9e1476b88ce9e5a81d386383074e
rimsha72 <mishaasad72@gmail.com>
2024-03-04 17:53:12 +0500

    changes

--- src/components/Home/Coupons.jsx
+++ src/components/Home/Coupons.jsx
@@ -4,6 +4,10 @@ import zara2 from "../../assets/zara2.png";
 import barcode from "../../assets/barcode.png";
 import barcode2 from "../../assets/barcode2.png";
 import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
+import voucher from "../../assets/voucher.png";
+import bg from "../../assets/bg.png";
+import club from "../../assets/club5.png";
+import club1 from "../../assets/club6.png";
 const Coupons = () => {
   return (
     <div className="lg:py-16 border-gray border-b text-black">
@@ -31,7 +35,7 @@ const Coupons = () => {
           </div>
         </div>
       </div>
-      <div className="flex lg:flex-row flex-col lg:gap-16 gap-4 pt-8">
+      {/* <div className="flex lg:flex-row flex-col lg:gap-16 gap-4 pt-8">
         <div className="relative flex w-full flex-col overflow-hidden rounded-xl drop-shadow">
           <a className="flex h-80 rounded-xl bg-white items-center " href="#">
             <div className="absolute top-4 px-4 w-full ">
@@ -151,7 +155,36 @@ const Coupons = () => {
           </a>
         </div>
         
-      </div>
+      </div> */}
+        <div className="grid lg:grid-cols-3 grid-cols-1 gap-x-4 gap-y-8">
+          <div>
+            <div
+              className="w-full md:max-w-sm p-4 relative h-32 md:h-[131px] bg-no-repeat lg:bg-contain bg-cover mb-4"
+              style={{ backgroundImage: `url(${bg})` }}
+            >
+              <div className="absolute top-0 left-0 ml-4 md:mt-0">
+                <img
+                  src={voucher}
+                  alt="voucher"
+                  className="w-48 h-20 md:w-[193px] md:h-[81px]"
+                />
+              </div>
+              <div className="flex flex-row md:absolute lg:bottom-4 bottom-6 md:gap-6 items-center text-white">
+                <div className="font-bold text-xs md:text-sm mt-24 md:mt-0">
+                  Voucher
+                </div>
+                <p className="text-xs px-2 py-1 border border-white rounded-full lg:mt-0 mt-6">
+                  Use until 15 July 2023
+                </p>
+              </div>
+              <div className="absolute lg:bottom-8 lg:right-2 p-2 text-white text-center">
+                <p className="font-medium text-base md:text-lg">30%</p>
+                <span className="text-xs md:text-sm">5 days left</span>
+              </div>
+            </div>
+          </div>
+         
+        </div>
       <div className="flex items-center justify-center my-12">
         <button className="bg-blue-350 w-[250px] h-[56px] rounded text-white">
           View All Companies
